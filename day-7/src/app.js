3d58882025ff6042d5628a71f1f2e50e6f1ddf7f
azit-rana0 <ajeetrana520@gmail.com>
2026-02-01 16:12:24 +0530

    feat: implement notes API with create and fetch functionality

--- day-7/src/app.js
+++ day-7/src/app.js
@@ -1,10 +1,35 @@
 const express = require('express')
+const noteModel = require('./models/notes.modal')
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use(express.json())
+
+// POST /note
+// req.body => {title, description}
+app.post('/notes', async (req, res) => {
+    const { title, description } = req.body
+
+    const notes = await noteModel.create({
+        title, description
+    })
+
+    console.log(notes)
+
+    res.status(201).json({
+        massage: 'Note created successfully',
+        notes: notes
+    })
+})
+
+// GET /note
+// fetch all the notes data
+app.get('/notes', async (req, res) => {
+
+    const notes = await noteModel.find()
     res.status(200).json({
-        massage: "Calling Successfully"
+        massage: "Noted fetched successfully",
+        notes: notes
     })
 })
 
